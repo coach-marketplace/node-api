52bb95f47cd59370ecbf04471b10512de9ae850a
Kevin <kevin.tassi@gmail.com>
2019-10-24 14:45:58 +0200

    Use utils for hashing and jwt

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,8 +1,7 @@
 'use strict'
 
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
+const { encryptString, isMatching } = require('../../_utils/hashing')
+const { getSignedToken } = require('../../_utils/jwt')
 const userQueries = require('../user/queries.js')
 
 module.exports = {
@@ -16,17 +15,12 @@ module.exports = {
       if (user) {
         throw new Error('This email is already used')
       }
-      bcrypt.hash(password, 10, async (error, hash) => {
-        if (error) {
-          throw new Error('Hash error: ' + error)
-        } else {
-          const newUser = await userQueries.create({
-            email,
-            password: hash,
-          })
-          res.status(201).json(newUser)
-        }
+      const hashedPassword = await encryptString(password)
+      const newUser = await userQueries.create({
+        email,
+        password: hashedPassword,
       })
+      res.status(201).json(newUser)
     } catch (error) {
       res.status(500).json({
         public_message: 'Error while trying to sign up',
@@ -45,28 +39,12 @@ module.exports = {
       if (!user) {
         throw new Error('Email or passord incorrect (1)')
       }
-      bcrypt.compare(password, user.password, (error, isPasswordCorrect) => {
-        console.log('++', error)
-        console.log('++2', isPasswordCorrect)
-        if (error || !isPasswordCorrect) {
-          res.status(401).json({
-            public_message: 'Email or password invalid',
-            debug_message: 'Email or passord incorrect (2)',
-          })
-        } else {
-          const token = jwt.sign(
-            {
-              email: user.email,
-              userId: user._id,
-            },
-            process.env.JWT_SECRET,
-            {
-              expiresIn: '1h',
-            },
-          )
-          res.status(201).json({ token })
-        }
-      })
+      const isMatch = await isMatching(password, user.password)
+      if (!isMatch) {
+        throw new Error('Email or passord incorrect (2)')
+      }
+      const token = getSignedToken({ email: user.email, userId: user._id })
+      res.status(201).json({ token })
     } catch (error) {
       res.status(500).json({
         public_message: 'Email or password invalid',
