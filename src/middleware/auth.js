701f18376c4f78992b8b3cea2ff97ee34fea7fc6
Kevin <kevin.tassi@gmail.com>
2019-11-17 09:43:21 +0100

    Refactor auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,16 +2,18 @@
 
 const { verifyToken } = require('../_utils/jwt')
 
-module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1]
-    const decoded = verifyToken(token)
-    req.authUser = decoded
-    next()
-  } catch (error) {
-    res.status(401).json({
-      public_message: 'Unauthorized',
-      debug_message: error.message,
-    })
-  }
+module.exports = {
+  isAuth: (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      const decoded = verifyToken(token)
+      req.authUser = decoded
+      next()
+    } catch (error) {
+      res.status(401).json({
+        public_message: 'Unauthorized',
+        debug_message: error.message,
+      })
+    }
+  },
 }
