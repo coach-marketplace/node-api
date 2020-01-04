853081cf0768af03a9acae865281c9ad5c6ddfb0
Ibrahim Dogrusoz <idogrusoz@gmail.com>
2020-01-04 23:05:13 +0100

    queries for sport endpoint

---
+++ src/controllers/sport/queries.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+
+const Sport = require('../../models/user')
+
+module.exports = {
+  getSports() {
+    return Sport.find()
+      .select('_id name')
+      .exec()
+  },
+
+  getUserById(sportId) {
+    return Sport.find({ _id: sportId })
+      .select('_id name')
+      .exec()
+  },
+
+  getSportByName(sportName) {
+    return Sport.find({ name: sportName })
+      .select('_id name')
+      .exec()
+  },
+
+  addSport(newSportData) {
+    const newSport = new Sport({
+      _id: new mongoose.Types.ObjectId(),
+      name: newSportData.name,
+    })
+    return newSport.save()
+  },
+
+  updateSport(sportId, newSportData) {
+    return Sport.findOneAndUpdate({ _id: sportId }, newSportData, {
+      new: true,
+    })
+  },
+
+  deleteSport(sportId) {
+    return Sport.deleteOne({ _id: { $eq: sportId } })
+  },
+}
