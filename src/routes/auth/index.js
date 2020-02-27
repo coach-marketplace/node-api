3f20a73f19ad1b4a4187dc7e1103bd2f431e0307
Kevin Tassi <kevin@wooclap.com>
2020-02-27 22:17:14 +0100

    Fix synthax

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -9,12 +9,12 @@ authRouter
   .get('/me', authJWT, getMe)
   .post('/register', register)
   .post('/login', authLocal, login)
-  // .get('/login-google', authGoogle)
-  // .get('/google/callback', authGoogle, (req, res) => {
-    // console.log('USER', req.user)
-    // console.log('++', req.headers)
-    // console.log('++', req.body)
-    // res.redirect('http://localhost:3000/')
-  })
+// .get('/login-google', authGoogle)
+// .get('/google/callback', authGoogle, (req, res) => {
+// console.log('USER', req.user)
+// console.log('++', req.headers)
+// console.log('++', req.body)
+// res.redirect('http://localhost:3000/')
+// })
 
 module.exports = authRouter
