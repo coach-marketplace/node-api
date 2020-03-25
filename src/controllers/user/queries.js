b557f36925c0d2c0c8ba9dec5a379ba041168bd1
Kevin Tassi <kevin.tassi@gmail.com>
2020-03-25 23:36:57 +0100

    imp: remove the expose field (temporary)

--- src/controllers/user/queries.js
+++ src/controllers/user/queries.js
@@ -8,11 +8,13 @@ mongoose.set('useFindAndModify', false)
 const User = require('../../models/user')
 
 const getExposedFields = (options = {}) => {
-  let fieldToExpose = '_id email first_name last_name accounts'
-  if (options.withPassword) {
-    fieldToExpose += ' password'
-  }
-  return fieldToExpose
+  // TODO: Filter which data are exposed -> https://mongoosejs.com/docs/api.html#query_Query-select
+  // let fieldToExclude = '-password'
+  // let fieldToExpose = '_id email firstName lastName accounts'
+  // if (options.withPassword) {
+  //   fieldToExpose += ' password'
+  // }
+  return '' //fieldToExpose
 }
 
 module.exports = {
@@ -37,18 +39,18 @@ module.exports = {
   create(data) {
     const newUserData = {
       _id: new mongoose.Types.ObjectId(),
-      email: data.email,
+      ...data,
     }
-    data.first_name && (newUserData.first_name = data.firstName)
-    data.last_name && (newUserData.last_name = data.lastName)
-    if (data.password) {
-      const account = {
-        type: USER_ACCOUNT_TYPE.LOCAL,
-        password: data.password,
-      }
-      newUserData.accounts = [account]
-      console.log('accounts', newUserData)
-    }
+    // data.first_name && (newUserData.first_name = data.firstName)
+    // data.last_name && (newUserData.last_name = data.lastName)
+    // if (data.password) {
+    //   const account = {
+    //     type: USER_ACCOUNT_TYPE.LOCAL,
+    //     password: data.password,
+    //   }
+    //   newUserData.accounts = [account]
+    //   console.log('accounts', newUserData)
+    // }
     // TODO: set google is exist
     // if (data.google_id) {
     //   const account = [{ type:id: data.google_id }]
@@ -61,10 +63,21 @@ module.exports = {
     return newUser.save()
   },
 
+  save(newUserData) {
+    newUserData._id = new mongoose.Types.ObjectId()
+    const newUser = new User(newUserData)
+
+    return newUser.save()
+  },
+
   editUser(userId, newUserData) {
     return User.findOneAndUpdate({ _id: userId }, newUserData, {
       new: true,
     })
+  },
+
+  delOne(query) {
+    return User.deleteOne(query)
   },
 
   removeUserById(userId) {
