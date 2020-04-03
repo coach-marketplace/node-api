af9d6fcc832ba23fd906615323d77c04a21df088
Kevin Tassi <kevin.tassi@gmail.com>
2020-04-03 22:52:07 +0200

    prevent getLocation to crash

--- src/controllers/geo-spatial/queries.js
+++ src/controllers/geo-spatial/queries.js
@@ -9,11 +9,18 @@ const axios = require('axios')
  */
 
 module.exports = {
-  getLocation: async query => {
-    const response = await axios.get(
-      `https://nominatim.openstreetmap.org/search/q=${query}?format=json`,
-    )
+  getLocation: async (query) => {
+    try {
+      const response = await axios.get(
+        `https://nominatim.openstreetmap.org/search/q=${query}?format=json`,
+      )
 
-    return response.data
+      return response.data
+    } catch (error) {
+      log('Error', { color: 'red' })
+      log(error.message)
+
+      return []
+    }
   },
 }
