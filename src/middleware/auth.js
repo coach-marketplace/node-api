73382ebae3ebb52569a938fec3171170f12634e2
Kevin Tassi <kevin.tassi@gmail.com>
2020-04-04 12:36:20 +0200

    Create requireAccessMyData middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -26,4 +26,32 @@ module.exports = {
       return next()
     })(req, res, next)
   },
+
+  /**
+   * This middleware will allow only the auth user to pass
+   * e.g. 'user-one' want to access his profile
+   * - He will make a request on `user/:id/profile`
+   * - We check is :id === authUser._id
+   *
+   * Note: This middleware assume that you are logged using
+   * requireJWTAuth middleware before
+   */
+  requireAccessMyData: (req, res, next) => {
+    if (!req.params.id) {
+      res.status(401).json({ message: 'Unauthorized' })
+      return
+    }
+
+    if (!req.user) {
+      res.status(401).json({ message: 'Unauthorized' })
+      return
+    }
+
+    if (req.user._id !== req.params.id) {
+      res.status(401).json({ message: 'Unauthorized' })
+      return
+    }
+
+    next()
+  },
 }
