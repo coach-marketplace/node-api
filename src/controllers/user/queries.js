fbf570d4a8995039a7f344fe732e6afb742d1c0e
Kevin Tassi <kevin.tassi@gmail.com>
2020-04-25 14:17:58 +0200

    Add todo task

--- src/controllers/user/queries.js
+++ src/controllers/user/queries.js
@@ -25,6 +25,9 @@ module.exports = {
     return newUser.save()
   },
 
+  // TODO: replace by findAndModify du to deprecation warning
+  // https://mongoosejs.com/docs/deprecations.html
+  // https://www.journaldev.com/6221/mongodb-findandmodify-example
   /**
    * @param {string} userId Id of the user to update
    * @param {object} newData Data to update
