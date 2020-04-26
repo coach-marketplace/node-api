d435fdbbfc0228b61b4ba6bfae98a1df8c351db4
Kevin Tassi <kevin.tassi@gmail.com>
2020-04-26 20:45:53 +0200

    chore: document lang handler

--- src/controllers/lang/handlers.js
+++ src/controllers/lang/handlers.js
@@ -2,10 +2,16 @@
 
 const { read } = require('./queries')
 
-module.exports = {
-  getLangByISO: async (iso) => {
-    const lang = (await read({ ISO_639_1: iso }))[0]
+/**
+ * @param {string} iso Lang ISO-639-1 ('en', 'fr',...)
+ * @return {object} Lang
+ */
+const getLangByISO = async (iso) => {
+  const lang = (await read({ ISO_639_1: iso }))[0]
 
-    return lang
-  },
+  return lang
+}
+
+module.exports = {
+  getLangByISO,
 }
